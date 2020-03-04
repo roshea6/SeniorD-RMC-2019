2b252dc8e019b5320cf627f4c6efe8bcd5ea8b7e
Ryan O'Shea <rpo1202@gmail.com>
2020-03-03 19:57:15 -0500

    Updated from testing with the odrive

--- src/odrive_control/src/odrive_demo_py3.py
+++ src/odrive_control/src/odrive_demo_py3.py
@@ -29,7 +29,7 @@ my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
 
 # Or to change a value, just assign to the property
-my_drive.axis0.controller.pos_setpoint = 3.14
+my_drive.axis0.controller.pos_setpoint = 1.72
 print("Position setpoint is " + str(my_drive.axis0.controller.pos_setpoint))
 
 # And this is how function calls are done:
--- src/odrive_control/src/odrive_test.py
+++ src/odrive_control/src/odrive_test.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 import odrive
+from odrive.enums import *
 
-print "Connecting to Odrive..."
+print("Connecting to Odrive...")
 
 # Find an available odrive device. This function will hold until a device is found
 odrv0 = odrive.find_any()
 
 # Print the voltage to make sure we're actually connected
 print(str(odrv0.vbus_voltage))
+
+
+odrv0.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+
+odrv0.axis0.controller.vel_setpoint = 5000
+
