82674ddf8547c4cead8e070ee112d4bd52aa37c5
Ryan O'Shea <rpo1202@gmail.com>
2020-03-11 01:24:22 -0400

    Updated readme with easily pasteable commands for both axis. Added initiliaztion for both axis and decreased speed to deal with current situation problem

--- src/odrive_control/README.md
+++ src/odrive_control/README.md
@@ -13,4 +13,18 @@ ODrive parameters for NEO motor:
 	odrv0.axis0.motor.config.current_lim_tolerance = 30
 	odrv0.axis0.motor.config.requested_current_range = 100
 	odrv0.axis0.motor.config.motor_type = 0
+
+
+	or
+
+	odrv0.axis1.controller.config.vel_limit = 2000
+	odrv0.axis1.controller.config.vel_limit_tolerance = 0
+	odrv0.axis1.controller.config.vel_gain = .5
+	odrv0.axis1.controller.config.vel_integrator_gain = .001
+	odrv0.axis1.encoder.config.cpr = 28
+	odrv0.axis1.encoder.config.bandwidth = 1000
+	odrv0.axis1.motor.config.current_lim = 30
+	odrv0.axis1.motor.config.current_lim_tolerance = 30
+	odrv0.axis1.motor.config.requested_current_range = 100
+	odrv0.axis1.motor.config.motor_type = 0
 	
--- src/odrive_control/src/odrive_test.py
+++ src/odrive_control/src/odrive_test.py
@@ -11,14 +11,15 @@ from controller_teleop.msg import robot_cmd
 
 # Controls the motor attached to the odrive based on incoming ROS messages
 def dtCallback(cmd_msg):
-	vel = 1500 * cmd_msg.dt_linear.x
+	vel = 600 * cmd_msg.dt_linear.x
 
 	print vel
 
 	odrv0.axis0.controller.vel_setpoint = vel
+	odrv0.axis1.controller.vel_setpoint = vel
 
 if __name__ == "__main__":
-	print("Connecting to Odrive...")
+	print("Connecting to Odrive 0")
 
 	# Find an available odrive device. This function will hold until a device is found
 	odrv0 = odrive.find_any(serial_number="2060387E304E")
@@ -29,7 +30,7 @@ if __name__ == "__main__":
 
 	# Calibrate the system
 	# TODO: Take out and set pre_calibrated tag to true
-	print("starting calibration...")
+	print("starting calibration of M0")
 	odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 	while odrv0.axis0.current_state != AXIS_STATE_IDLE:
 		time.sleep(0.1)
@@ -40,6 +41,21 @@ if __name__ == "__main__":
 
 	# Request the odrive to enter velocity control mode
 	odrv0.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+
+	# Calibrate the system
+	# TODO: Take out and set pre_calibrated tag to true
+	print("starting calibration of M1")
+	odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+	while odrv0.axis1.current_state != AXIS_STATE_IDLE:
+		time.sleep(0.1)
+
+	# Set state to closed loop control
+	# In this state the odrive will try to maintain whatever state you send it
+	odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
+	# Request the odrive to enter velocity control mode
+	odrv0.axis1.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+
 
 	# Initiliaze the ROS node
 	rospy.init_node('drivetrain_control', anonymous=True)
